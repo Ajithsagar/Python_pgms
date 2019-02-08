c53b7f6451df70bbb0e0211ea578177cdd9633eb
Ajithsagar <ajithsagar26062000@gmail.com>
2019-02-08 20:25:00 +0530

    2nd commit

---
+++ sum_of_matrix.py
@@ -0,0 +1,28 @@
+def read(m,n):
+    L=[]
+
+    for i in range (m):
+        r=[]
+        for j in range (n):
+            a=int(input('Enter the number:'))
+            r.append(a)
+        L.append(a) 
+    return L
+def add(c,c1,m,n):
+    L=[]
+    for i in range (m):
+        r=[]
+        for j in range (n):
+            sum=0
+            sum=c[i][j]+c1[i][j],
+
+            r.append(a)
+            print()
+        L.a[ppend(r)
+return (L)
+m=int(input('Enter the number of rows:
+n=int(input('Enter the number of columns:'))
+c=read(m,n)
+c1=read(m,n)
+S=add(c,c1,m,n)
+print(S)
